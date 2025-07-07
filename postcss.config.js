ee312b1723f382d3531bc24b0046e721abe47fee
Harold-198766 <lic.barillas46@gmail.com>
2025-07-06 19:34:34 -0600

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,5 @@
-const purgecss = require('@fullhuman/postcss-purgecss');
-
 module.exports = {
   plugins: [
-    require('autoprefixer'),
-    purgecss({
-      content: ['./*.html', './articulos/**/*.md', './admin/**/*.html'],
-      defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-    }),
-  ],
+    require('autoprefixer')
+  ]
 };
