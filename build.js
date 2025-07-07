ee99fd90abc5adbdd2a7c75b72be5450912625bf
Harold-198766 <lic.barillas46@gmail.com>
2025-07-06 19:44:08 -0600

    Add files via upload

---
+++ build.js
@@ -0,0 +1,8 @@
+require('esbuild').build({
+  entryPoints: ['scripts/main.js'],
+  bundle: true,
+  minify: true,
+  treeShaking: true,
+  outfile: 'dist/main.min.js',
+  target: ['es2020'],
+}).catch(() => process.exit(1));
