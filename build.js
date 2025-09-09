762a7e0085ae40a5619bf67a1eaf8fe37adab23b
Harold-198766 <lic.barillas46@gmail.com>
2025-09-08 20:39:50 -0600

    Update build.js

--- build.js
+++ build.js
@@ -1,7 +1,5 @@
 require('esbuild').build({
   entryPoints: ['scripts/main.js'],
-  "build:html": "cp index.html dist/index.html"
-  "build": "npm run build:css && npm run build:js && npm run build:html"
   bundle: true,
   minify: true,
   treeShaking: true,
